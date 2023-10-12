025c19ad6a5ce3a8d72ac06e7e61d9f2d4476e5b
Conor Turner <conor_turner@hotmail.com>
2023-10-12 13:26:23 +0100

    dictionary cracker working

--- password_breakers.py
+++ password_breakers.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 from concurrent.futures import ProcessPoolExecutor
+from itertools import combinations
 import os
 
 
@@ -8,6 +8,12 @@ class PasswordCrackerBase:
     def __init__(self):
         self.chars = []
         self._length = 0
+
+        # for multi-processing, need a way to communicate with other processes 
+        # to let them know if another process has found the solution
+        self.stop_path = './stop.txt'
+        if os.path.exists(self.stop_path):
+            os.remove(self.stop_path)
 
     def _update_length(self):
         self._length = len(self.chars)
@@ -31,18 +37,6 @@ class PasswordCrackerBase:
     def add_symbols(self):
         self.chars.extend(list('!@£$%^&*()€#,./;\'\\[]{}:"|?><'))
         self._update_length()
-        
-
-class BruteForcePasswordCracker(PasswordCrackerBase):
-
-    def __init__(self):
-        super().__init__()
-        # for multi-processing, need a way to communicate with other processes 
-        # to let them know if another process has found the solution
-        self.stop_path = './stop.txt'
-
-        if os.path.exists(self.stop_path):
-            os.remove(self.stop_path)
 
     def set_password(self, password):
         if not self.chars:
@@ -53,16 +47,7 @@ class BruteForcePasswordCracker(PasswordCrackerBase):
                 raise ValueError(f'Character {letter} not allowed')
 
         self.password = password
-
-    def _get_guess(self, i):
-        idxs = []
-        while i >= self._length:
-            idxs.append(i % self._length)
-            i = int(i / self._length) - 1
-        idxs.append(i % self._length)
-        output = ''.join([self.chars[idx] for idx in idxs])
-        return output
-    
+        
     def _check_stop(self):
         return os.path.exists(self.stop_path)
 
@@ -72,6 +57,18 @@ class BruteForcePasswordCracker(PasswordCrackerBase):
 
     def _remove_stop(self):
         return os.remove(self.stop_path)
+
+
+class BruteForcePasswordCracker(PasswordCrackerBase):
+
+    def _get_guess(self, i):
+        idxs = []
+        while i >= self._length:
+            idxs.append(i % self._length)
+            i = int(i / self._length) - 1
+        idxs.append(i % self._length)
+        output = ''.join([self.chars[idx] for idx in idxs])
+        return output
 
     def crack_password(self, i=0, inc=1):
         guess = ''
@@ -83,11 +80,88 @@ class BruteForcePasswordCracker(PasswordCrackerBase):
             count += 1
             if count % 1000 == 0:
                 if self._check_stop():
+                    self.check_count = count
                     return
 
         self._write_stop(guess)
+        self.check_count = count
         return guess
     
+    def crack_password_multiprocess(self, workers=1):
+        with ProcessPoolExecutor(max_workers=workers) as pool:
+            for i in range(workers):
+                pool.submit(self.crack_password, i, workers)
+
+        with open(self.stop_path) as f:
+            password = f.read()
+        return password
+
+
+class DictionaryPasswordCracker(PasswordCrackerBase):
+
+    def __init__(self, passwords_path='./passwords.txt'):
+        super().__init__()
+        with open(passwords_path) as f:
+            passwords = f.read().split('\n')[1:]
+        passwords = [password.strip() for password in passwords if password]
+        self.passwords = passwords
+
+        self.substitutions = {char: [char.upper()] for char in list('abcdefghijklmnopqrstuvwxyz')}
+        for char, sub in (
+            ('a', '4'), ('e', '3'), ('i', '1'), ('o', '0'), ('s', 'S'),
+        ):
+            self.substitutions[char].append(sub)
+
+    def _get_idxs(self, string, char):
+        return [i for i,ichar in enumerate(string) if ichar==char]
+    
+    def _get_combinations(self, items, length=0):
+        all_combinations = []
+        lengths = range(1, len(items) + 1) if length == 0 else [length]
+        for r in lengths:
+            all_combinations.extend(combinations(items, r))
+        return all_combinations
+    
+    @staticmethod
+    def _replace(string, idx, char):
+        if idx == len(string)-1:
+            return f'{string[:-1]}{char}'
+        elif idx == 0:
+            return f'{char}{string[1:]}'
+        else:
+            return f'{string[:idx]}{char}{string[idx+1:]}'
+    
+    def _replace_all(self, string, idxs, char):
+        for idx in idxs:
+            string = self._replace(string, idx, char)
+        return string
+
+    def _get_variations(self, password):
+        yield password
+        for char, subs in self.substitutions.items():
+            idxs = self._get_idxs(password, char)
+            if idxs:
+                combis = self._get_combinations(idxs)
+                for combi in combis:
+                    for sub in subs:
+                        new_password = self._replace_all(password, combi, sub)
+                        yield new_password
+
+    def crack_password(self, i=0, inc=1):
+        count = 0
+        for password in self.passwords[i::inc]:
+            for variation in self._get_variations(password):
+                count += 1
+                if variation == self.password:
+                    self._write_stop(variation)
+                    self.check_count = count
+                    return variation
+                
+                if count % 1000 == 0:
+                    if self._check_stop():
+                        self.check_count = count
+                        return
+
     def crack_password_multiprocess(self, workers=1):
         with ProcessPoolExecutor(max_workers=workers) as pool:
             for i in range(workers):
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from password_breakers import BruteForcePasswordCracker
+from password_breakers import BruteForcePasswordCracker, DictionaryPasswordCracker
 
 
 class TestBruteForce(unittest.TestCase):
@@ -35,3 +35,82 @@ class TestBruteForce(unittest.TestCase):
         self.cracker._update_length()
         self.cracker.set_password('bacca')
         self.assertEqual(self.cracker.crack_password_multiprocess(), 'bacca')
+
+
+class TestDictionary(unittest.TestCase):
+
+    def setUp(self):
+        self.cracker = DictionaryPasswordCracker()
+
+    def test_get_idxs(self):
+        self.assertEqual([0, 2, 4], self.cracker._get_idxs('ababa', 'a'))
+        self.assertEqual([1, 3], self.cracker._get_idxs('ababa', 'b'))
+
+        self.assertEqual([1], self.cracker._get_idxs('ab', 'b'))
+
+        self.assertEqual([], self.cracker._get_idxs('ababa', 'c'))
+
+    def test_get_combinations(self):
+        combis = self.cracker._get_combinations([1, 2, 3])
+        for expected in (
+            (1, ), (2, ), (3, ),
+            (1, 2), (1, 3), (2, 3),
+            (1, 2, 3)
+        ):
+            self.assertIn(expected, combis)
+
+    def test_get_combinations_fixed_length(self):
+        combis = self.cracker._get_combinations([1, 2, 3], 2)
+        for expected in (
+            (1, 2), (1, 3), (2, 3),
+        ):
+            self.assertIn(expected, combis)
+
+    def test_replace(self):
+        self.assertEqual('abz', self.cracker._replace('abc', 2, 'z'))
+        self.assertEqual('zbc', self.cracker._replace('abc', 0, 'z'))
+        self.assertEqual('azc', self.cracker._replace('abc', 1, 'z'))
+
+    def test_replace_all(self):
+        self.assertEqual('zzz', self.cracker._replace_all('abc', [0, 1, 2], 'z'))
+
+    def test_get_variations(self):
+        password = 'ababa'
+        self.cracker.substitutions = {'b': ['c']}
+        combinations = list(self.cracker._get_variations(password))
+
+        for expected in (
+            'ababa', 'acaba', 'abaca', 'acaca'
+        ):
+            self.assertIn(expected, combinations)
+
+    def test_get_variations2(self):
+        password = 'abcabc'
+        self.cracker.substitutions = {'a': ['z', 'y']}
+        combinations = list(self.cracker._get_variations(password))
+
+        for expected in (
+            'abcabc', 
+            'zbcabc', 'abczbc', 'zbczbc',
+            'ybcabc', 'abcybc', 'ybcybc',
+            #'zbcybc', 'ybczbc',  # code does not do combinations of substitutions (yet)
+        ):
+            self.assertIn(expected, combinations)
+
+    def test_crack_password(self):
+        self.cracker.add_lowers()
+        self.cracker.add_uppers()
+        self.cracker.add_numbers()
+        self.cracker.set_password('P4ssw0rd')  # 'password' with P, a and 0 substituted
+        self.assertEqual(self.cracker.crack_password(), 'P4ssw0rd')
+
+    def test_crack_password_multiprocessing(self):
+        self.cracker.add_lowers()
+        self.cracker.add_uppers()
+        self.cracker.add_numbers()
+        self.cracker.set_password('P4ssw0rd')  # 'password' with P, a and 0 substituted
+        self.assertEqual(self.cracker.crack_password_multiprocess(), 'P4ssw0rd')
+
+
+if __name__ == '__main__':
+    unittest.main()
